6467777074d39c0c24cc4bb721372644478547de
abdul98786 <30839512+abdul98786@users.noreply.github.com>
2017-08-15 15:59:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text'
     );
 element.innerHTML = 'New Value';
 //move the image
-var img = document.getElementById('modi');
+var img = document.getElementById('nodi');
 img.onclick = function() {
     img.style.marginLeft = '100px';    
 };
