5dbcd55a74582f4fa42e7a8c65bedbb9952ee804
abdul98786 <30839512+abdul98786@users.noreply.github.com>
2017-08-15 16:01:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text'
     );
 element.innerHTML = 'New Value';
 //move the image
-var img = document.getElementById('aodi');
+var img = document.getElementById('modi');
 img.onclick = function() {
     img.style.marginLeft = '100px';    
 };
